da32eae04c06f79446df67ad9692d35bc88e897a
卜星星 <boxingxing@limikeji.com>
2021-01-22 19:54:26 +0800

    获取用户

--- utils/ldap_cli.py
+++ utils/ldap_cli.py
@@ -9,14 +9,18 @@ class LdapCli:
 
     def __init__(self, host, root_dn, username, password):
         self.host = host
+        self.username = username
+        self.password = password
         self.root_dn = root_dn
         self.group_dn = f'cn=group,{self.root_dn}'
         self.member_dn = f'cn=member,{self.root_dn}'
         self.server = Server(self.host)
-        self.conn = Connection(self.server, f'cn={username},{self.root_dn}', password, auto_bind=True)
 
     def _gen_password(self):
         return '123456'
+
+    def _create_connection(self):
+        return Connection(self.server, f'cn={self.username},{self.root_dn}', self.password, auto_bind=True)
 
     def add_user(self, username, name):
         '''
@@ -29,7 +33,28 @@ class LdapCli:
             'uid': f'{username}',
             'userPassword': self._gen_password(),
         }
-        return self.conn.add(dn, object_class, attribute)
+        conn = self._create_connection()
+        return conn.add(dn, object_class, attribute)
+
+    def get_users(self):
+        '''
+        获取用户列表
+        '''
+        dn = 'cn=member,dc=waylonglong,dc=com'
+        conn = self._create_connection()
+        attributes = ['cn', 'sn']
+        conn.search(dn, '(objectclass=person)', attributes=attributes)
+        data_list = []
+        for user in conn.entries:
+            user = user.entry_attributes_as_dict
+            data = {
+                'username': user['cn'][0],
+                'name': user['sn'][0],
+            }
+            data_list.append(data)
+        return data_list
+
+
 
 
 ldap_cli = LdapCli('ldap://127.0.0.1:389', 'dc=waylonglong,dc=com', 'admin', 'ldap123')
